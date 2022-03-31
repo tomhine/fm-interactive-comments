805a797992a377be157796fcc343a756a9b2c252
Tom Hine <tomhine792@gmail.com>
2022-03-31 15:01:29 +0100

    rough out container layout

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,11 @@ const comments = useCommentsStore();
 </script>
 
 <template>
-  <ul>
-    <li v-for="comment in comments.getComments()" :key="comment.id">
-      {{ comment.content }}
-    </li>
-  </ul>
+  <main class="mx-auto mt-4 w-full max-w-3xl lg:mt-11 lg:p-4">
+    <ul class="space-y-4 lg:space-y-5">
+      <li v-for="comment in comments.getComments()" :key="comment.id">
+        {{ comment.content }}
+      </li>
+    </ul>
+  </main>
 </template>
